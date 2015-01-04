5dbbd3d65b0f598c20144ee930cf3bb8083c4805
Ryan <mymailzhong@qq.com>
2015-01-04 19:31:49 +0800

    151 reverse words

---
+++ 151_reverse_words.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+using namespace std;
+
+class Solution {
+	public:
+		void reverseWords(string &s) {
+			istringstream is(s);
+			string tmp="";
+			string out="";
+			while(is>>tmp){
+				tmp+=" ";
+				tmp+=out;
+				out=tmp;
+			}
+			s=out.substr(0,out.length()-1);
+			cout<<"s: "<<s.length()<<endl;
+		}
+};
+
+int main()
+{
+	string s;
+	Solution solu;
+	getline(cin,s);
+
+	solu.reverseWords(s);
+	return 0;
+}
