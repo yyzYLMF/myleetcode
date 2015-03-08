0af7905080906e994fdee4b84ab5fdef2bf531a3
Ryan <mymailzhong@qq.com>
2015-03-08 20:44:59 +0800

    edit leetcode-4 and add summary

--- 4_median_of_two_sorted_arrays.cc
+++ 4_median_of_two_sorted_arrays.cc
@@ -14,6 +14,7 @@ using namespace std;
 #define M 5
 #define N 7
 
+//Although is AC, but this is not a right answer
 class Solution {
 	public:
 	double findMedianSortedArrays(int A[], int m, int B[], int n) {
@@ -47,6 +48,39 @@ class Solution {
 		else {
 			return (double)(((double)(ivec[ivec.size()/2-1]+ivec[ivec.size()/2]))/2.0);
 		}
+	}
+};
+
+//This is the right answer;
+class Solution2 {
+	public:
+	double findKth(int a[], int m, int b[], int n, int k)
+	{
+		//always assume that m is equal or smaller than n
+		if (m > n)
+			return findKth(b, n, a, m, k);
+		if (m == 0)
+			return b[k - 1];
+		if (k == 1)
+			return min(a[0], b[0]);
+		//divide k into two parts
+		int pa = min(k / 2, m), pb = k - pa;
+		if (a[pa - 1] < b[pb - 1])
+			return findKth(a + pa, m - pa, b, n, k - pa);
+		else if (a[pa - 1] > b[pb - 1])
+			return findKth(a, m, b + pb, n - pb, k - pb);
+		else
+			return a[pa - 1];
+	}
+
+	double findMedianSortedArrays(int A[], int m, int B[], int n)
+	{
+		int total = m + n;
+		if (total & 0x1)
+			return findKth(A, m, B, n, total / 2 + 1);
+		else
+			return (findKth(A, m, B, n, total / 2)
+					+ findKth(A, m, B, n, total / 2 + 1)) / 2;
 	}
 };
 
