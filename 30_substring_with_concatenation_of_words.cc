c96e5437cf38a1ba5183c34cdd61e7324b720dc4
Ryan <mymailzhong@qq.com>
2015-03-16 10:17:18 +0800

    edit 30.cc and summary

--- 30_substring_with_concatenation_of_words.cc
+++ 30_substring_with_concatenation_of_words.cc
@@ -61,6 +61,44 @@ class Solution {
 	}
 };
 
+class Solution2 {
+	public:
+	vector<int> findSubstring(string S, vector<string> &L) {
+		vector<int> ret;
+		int slen = S.length();
+		int wordsLen = L.size();
+		if (wordsLen == 0) {
+			for (int i = 0; i < slen; i++) {
+				ret.push_back(i);
+			}
+			return ret;
+		}
+		wordsLen *= L[0].length();
+		map<string, int> mp;
+		map<string, int> cur;
+		for (int i = 0; i < L.size(); i++) {
+			mp[ L[i] ]++;
+		}
+		for (int i = 0; i + wordsLen <= slen; i++) {
+			cur.clear();
+			bool flag = true;
+			for (int j = 0; j < L.size(); j++) {
+				string tmp = S.substr(i + j * L[0].length(), L[0].length());
+				if (mp[tmp] > cur[tmp]) {
+					cur[tmp]++;
+				} else {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) {
+				ret.push_back(i);
+			}
+		}
+		return ret;
+	}
+};
+
 int main() {
 	string s("barfoothefoobarman");
 	vector<string> input;
