6e6408c026f582940b37c1652703781cfbec7851
Ryan <mymailzhong@qq.com>
2015-03-17 10:13:47 +0800

    edit 32.cc and summary file

--- 32_longest_valid_parentheses.cc
+++ 32_longest_valid_parentheses.cc
@@ -69,6 +69,30 @@ class Solution {
 	}
 };
 
+//greed algorithm
+class Solution2 {
+	public:
+	stack<char> st;
+	stack<int> pos;
+	int ret = 0;
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == '(') {
+			st.push(s[i]);
+			pos.push(i);
+		} else {
+			if (!st.empty() && st.top() == '(') {
+				st.pop();
+				pos.pop();
+				ret = max(ret, (st.empty() ? i + 1 : i - pos.top()));
+			} else {
+				st.push(s[i]);
+				pos.push(i);
+			}
+		}
+	}
+	return ret;
+};
+
 int main() {
 	string s;
 	int result;
