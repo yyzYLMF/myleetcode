5428fb036edb90f1483dd0111ff9833253a51265
Ryan <mymailzhong@qq.com>
2015-09-01 15:35:24 +0800

    191

--- 191_number_of_1bits.cc
+++ 191_number_of_1bits.cc
@@ -10,7 +10,20 @@
 #include <cstdint>
 using namespace std;
 
+//BEST
 class Solution {
+    public:
+    int hammingWeight(uint32_t n) {
+        int ret=0;
+        while(n != 0) {
+            ret++;
+            n = n&(n-1);
+        }
+        return ret;
+    }
+};
+
+class Solution2 {
     public:
     int hammingWeight(uint32_t n) {
         int i,temp,count=0;
