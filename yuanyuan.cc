3bb1eae184f2ced40ed23d890e7c8f2e043c390f
Ryan <yyzYLMF@users.noreply.github.com>
2016-09-05 21:12:09 +0800

    Update yuanyuan.cc

--- yuanyuan.cc
+++ yuanyuan.cc
@@ -1,46 +1,28 @@
 #include <iostream>
 #include <cstdlib>
-#include <map>
-#include <vector>
-#include <string>
-#include <algorithm>
+#include <math>
 using namespace std;
 
 int main() {
-	int n,m,tmp,min,max;
-	string stmp;
-	vector<int> value;
-	vector<int> num;
-	map<string,int> mt;
-	
+	int n,m,max,day,value,lastd,lastv;
 	while(cin>>n>>m) {
-		value.clear();
-		num.clear();
-		mt.clear();
-		for(int i=0;i<n;++i) {
-			cin>>tmp;
-			value.push_back(tmp);
-		}
 		for(int i=0;i<m;++i) {
-			cin>>stmp;
-			mt[stmp]++;
+			cin>>day>>value;
+			if(i==0) {
+				lastd=day;
+				lastv=value;
+				max = day-1+value;
+			}
+			int tmp = abs(value-lastv)+(day-lastd)/2;
+			if(tmp>max)
+				max = tmp;
+			lastd = day;
+			lastv = value;
+		} 
+		int tmp = lastv + (n-lastd);
+		if(tmp > max) {
+			max = tmp;
 		}
-		map<string,int>::iterator it = mt.begin();
-		while(it != mt.end()) {
-			tmp = it->second;
-			num.push_back(tmp);
-			++it;
-		}
-		sort(value.begin(),value.end());
-		sort(num.begin(),num.end());
-		min = 0;
-		max = 0;
-		for(int i=num.size()-1,j=0; i>=0; --i,++j) {
-			min += num[i]*value[j];
-		}
-		for(int i=num.size()-1,j=value.size()-1; i>=0; --i,--j) {
-			max += num[i]*value[j];
-		}
-		cout<<min<<" "<<max<<endl;
+		cout<<max<<endl;
 	}
 }
