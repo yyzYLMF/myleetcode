91623d49b52da77dda0f44e96180a47599637103
Ryan <yyzYLMF@users.noreply.github.com>
2016-09-19 20:00:48 +0800

    Update yuanyuan.cc

--- yuanyuan.cc
+++ yuanyuan.cc
@@ -1,37 +1,82 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
-#include <cstring>
-#include <algorithm>
 using namespace std;
 
-class Solution {
+
+//模板类
+template <typename T,int num>
+class Queue
+{
+private:
+        T arr[num];
+        int front;
+        int rear;
 public:
-    int longestPrefixPalindrome(string s) {
-		int len = s.length();
-		if (len < 2)
-			return len;
-		int longest = 1, start, end;
-		for (int begin = 0; begin <= len / 2;) {
-			start = end = begin;
-			while (end < len - 1 && s[end + 1] == s[end])
-				++end;
-			begin = end + 1;
-			while (end < len - 1 && start > 0 && s[end + 1] == s[start - 1]) {
-				++end;
-				--start;
-			}
-			if (start == 0 && longest < end - start + 1)
-				longest = end - start + 1;
-		}
-		return longest;
-    }
+        Queue() //构造函数
+        {
+         front=0;
+         rear=0;
+        }
+
+        bool isEmpty();//判断队列是否为空
+        bool isFull();//判断队列是否为满
+        bool push(const T &);//进队列
+        bool pop(T &);//出队列 这里的参数一定是引用 因为要改变其本身
 };
 
-int main() {
-	Solution solu;
-	string str;
-	cin>>str;
-	cout<<solu.longestPrefixPalindrome(str)<<endl;
-	return 0;
+template <typename T,int  num>
+bool Queue<T,num>::isEmpty()
+{
+        return front==rear;
+}
+
+template<typename T,int num>
+bool Queue<T,num>::isFull()
+{
+        return front==num;
+}
+
+template<typename T,int num> //进队列 判断队列是否满
+bool Queue<T,num>::push(const T &a)
+{
+        if(isFull())
+        {
+         cout<<"队列已满"<<endl;
+         return false;
+        }
+        else
+        {
+         arr[front]=a;
+         ++front;
+        }
+        return true;
+}
+
+template<typename T,int num>
+bool Queue<T,num>::pop(T &b)
+{
+        if(isEmpty())
+        {
+         cout<<"队列空了"<<endl;
+         return false;
+        }
+        else
+        {
+         b=arr[rear];
+         ++rear;
+        }
+        return true;
+}
+
+int main()
+{
+        Queue<int,7> q;  //开7个空间
+        cout<<"刚开始队列空吗?  "<<q.isEmpty()<<endl;
+        q.push(6);
+        q.push(10);
+        cout<<"现在还是队列空吗?  "<<q.isFull()<<endl;
+        int b1,b2;
+        q.pop(b1);
+        q.pop(b2);
+        cout<<"队列中第一个删除的元素为:  "<<b1<<endl;
+        cout<<"队列中第二个删除的元素为:  "<<b2<<endl;
 }
